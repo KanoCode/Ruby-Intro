84f44de0e7dda777d613cf21adfc0f113966d2f4
KanoCode <kanokanombola@gmail.com>
2022-07-14 20:35:40 +0200

    Create file morse_decoder, Add solution

---
+++ morse_decoder.rb
@@ -0,0 +1,34 @@
+
+def decode(morse_str)
+    count_spaces = 0
+    word = ''
+    final_text = ''
+    character = ''
+  
+
+  morse_dictionary = {".-"=>"A","-..."=>"B","-.-."=>"C","-.."=>"D
+    ","."=>"E","..-."=>"F","--."=>"G","...."=>"H",".."=>"I", ".---"=>"J","-.-"=>"K",
+    ".-.."=>"L","--"=>"M","-."=>"N","---"=>"O",".--."=>"P","--.-"=>"Q",".-."=>"R","..."=>"S","-"=>"T","..-"=>"U","...-"=>"V",".--"=>"W","-..-"=>"X","-.--"=>"Y","--..
+    "=>"Z"}
+
+ word = ''
+final_text = ''
+
+morse_str.split("   ").each do |i|  
+  i.split(" ").each do |r|
+    word.concat(morse_dictionary[r])
+  end
+  final_text.concat(word+" ")
+  word =""
+    end
+return final_text
+end
+morse_str = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+decode(morse_str)
+
+    
+    # We will have a variable to track down the space characters
+
+    # if the space character >= 2 then we add the space to our word else we ignore it  
+# loop through each character in a morse_str if the next 
+
