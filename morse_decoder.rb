28ee2f7d798b28571eb2198548d5f69114f740c3
SimbaWANGU <simbawangulu@gmail.com>
2022-07-14 22:06:05 +0300

    Add linters to local environment

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,34 +1,29 @@
-
 def decode(morse_str)
-    count_spaces = 0
-    word = ''
-    final_text = ''
-    character = ''
-  
+  word = ''
+  final_text = ''
 
-  morse_dictionary = {".-"=>"A","-..."=>"B","-.-."=>"C","-.."=>"D
-    ","."=>"E","..-."=>"F","--."=>"G","...."=>"H",".."=>"I", ".---"=>"J","-.-"=>"K",
-    ".-.."=>"L","--"=>"M","-."=>"N","---"=>"O",".--."=>"P","--.-"=>"Q",".-."=>"R","..."=>"S","-"=>"T","..-"=>"U","...-"=>"V",".--"=>"W","-..-"=>"X","-.--"=>"Y","--..
-    "=>"Z"}
+  morse_dictionary = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+                       '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K',
+                       '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q',
+                       '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W',
+                       '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z' }
 
- word = ''
-final_text = ''
+  word = ''
+  final_text = ''
 
-morse_str.split("   ").each do |i|  
-  i.split(" ").each do |r|
-    word.concat(morse_dictionary[r])
-  end
-  final_text.concat(word+" ")
-  word =""
+  morse_str.split('   ').each do |i|
+    i.split.each do |r|
+      word.concat(morse_dictionary[r])
     end
-return final_text
+    final_text.concat("#{word} ")
+    word = ''
+  end
+  final_text
 end
-morse_str = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
-decode(morse_str)
+morse_str = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+p decode(morse_str)
 
-    
-    # We will have a variable to track down the space characters
+# We will have a variable to track down the space characters
 
-    # if the space character >= 2 then we add the space to our word else we ignore it  
-# loop through each character in a morse_str if the next 
-
+# if the space character >= 2 then we add the space to our word else we ignore it
+# loop through each character in a morse_str if the next
